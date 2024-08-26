e31615077bb5ff29a99e41552ef495ed5db61994
edsonalcala <edson_alcala@msn.com>
2024-08-26 17:48:23 +0100

    fix: first example working

--- examples/evm-0-simple-encoding/src/lib.rs
+++ examples/evm-0-simple-encoding/src/lib.rs
@@ -1,7 +1,8 @@
 use near_sdk::near;
-// use omni_transaction::transaction_builder::TransactionBuilder;
-// use omni_transaction::transaction_builder::TxBuilder;
-// use omni_transaction::types::EVM;
+use omni_transaction::evm::utils::parse_eth_address;
+use omni_transaction::transaction_builder::TransactionBuilder;
+use omni_transaction::transaction_builder::TxBuilder;
+use omni_transaction::types::EVM;
 
 #[near(contract_state)]
 pub struct Contract {}
@@ -14,33 +15,49 @@ impl Default for Contract {
 
 #[near]
 impl Contract {
-    pub fn get_transaction_encoded_data(&self) -> String {
-        // let tx = TransactionBuilder::new::<EVM>().nonce(1).build();
-        // .nonce(1)
-        // .(1)
-        // .set_gas_limit(1)
-        // .set_to("0x")
-        // .build();
+    pub fn get_transaction_encoded_data(&self) -> Vec<u8> {
+        let to_address_str = "d8dA6BF26964aF9D7eEd9e03E53415D37aA96045";
+        let to_address = parse_eth_address(to_address_str);
+        let max_gas_fee: u128 = 20_000_000_000;
+        let max_priority_fee_per_gas: u128 = 1_000_000_000;
+        let gas_limit: u128 = 21_000;
+        let chain_id: u64 = 1;
+        let nonce: u64 = 0;
+        let data: Vec<u8> = vec![];
+        let value: u128 = 10000000000000000; // 0.01 ETH
 
-        // tx.build_for_signing()
-        "Hello World!".to_string()
+        let evm_tx = TransactionBuilder::new::<EVM>()
+            .nonce(nonce)
+            .to(to_address)
+            .value(value)
+            .input(data.clone())
+            .max_priority_fee_per_gas(max_priority_fee_per_gas)
+            .max_fee_per_gas(max_gas_fee)
+            .gas_limit(gas_limit)
+            .chain_id(chain_id)
+            .build();
+
+        evm_tx.build_for_signing()
     }
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use near_sdk::log;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-//     use super::*;
+    #[test]
+    fn test_get_transaction_encoded_data() {
+        let contract = Contract::default();
 
-//     #[test]
-//     fn test_get_transaction_encoded_data() {
-//         let contract = Contract::default();
+        let encoded_data = contract.get_transaction_encoded_data();
 
-//         let encoded_data = contract.get_transaction_encoded_data();
+        let expected_data = vec![
+            2, 239, 1, 128, 132, 59, 154, 202, 0, 133, 4, 168, 23, 200, 0, 130, 82, 8, 148, 216,
+            218, 107, 242, 105, 100, 175, 157, 126, 237, 158, 3, 229, 52, 21, 211, 122, 169, 96,
+            69, 135, 35, 134, 242, 111, 193, 0, 0, 128, 192,
+        ];
 
-//         log!("encoded_data: {:?}", encoded_data);
-
-//         assert!(encoded_data.len() > 0);
-//     }
-// }
+        assert!(encoded_data.len() > 0);
+        assert_eq!(encoded_data, expected_data);
+    }
+}
